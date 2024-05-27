cefd577c7ff9411b9703395f3c4e090246f25afc
vinciuscastro <vinisilvacastro71@gmail.com>
2024-05-27 01:36:49 -0300

    Snack Bar confirmation

--- lib/widgets/cart_item_widget.dart
+++ lib/widgets/cart_item_widget.dart
@@ -4,6 +4,7 @@ import '../providers/cart.dart';
 
 class CartItemWidget extends StatelessWidget {
   final CartItem cartItem;
+
   const CartItemWidget(this.cartItem, {super.key});
 
   @override
@@ -25,8 +26,31 @@ class CartItemWidget extends StatelessWidget {
         ),
       ),
       direction: DismissDirection.endToStart,
+      confirmDismiss: (_) {
+        return showDialog(
+            context: context,
+            builder: (ctx) => AlertDialog(
+                  title: Text("Tem certeza?"),
+                  content: Text("Deseja remover o item do carrinho?"),
+                  actions: [
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop(false);
+                      },
+                      child: Text("Não"),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop(true);
+                      },
+                      child: Text("Sim"),
+                    ),
+                  ],
+                ));
+      },
       onDismissed: (_) {
-        Provider.of<Cart>(context, listen: false).removeItem(cartItem.productId);
+        Provider.of<Cart>(context, listen: false)
+            .removeItem(cartItem.productId);
       },
       child: Card(
         borderOnForeground: true,
@@ -37,11 +61,11 @@ class CartItemWidget extends StatelessWidget {
         child: ListTile(
           leading: CircleAvatar(
             backgroundColor: Theme.of(context).primaryColor,
-      
             child: Padding(
               padding: const EdgeInsets.all(5),
               child: FittedBox(
-                child: Text(cartItem.price.toStringAsFixed(2), style: TextStyle(color: Colors.white)),
+                child: Text(cartItem.price.toStringAsFixed(2),
+                    style: TextStyle(color: Colors.white)),
               ),
             ),
           ),
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -37,6 +37,19 @@ class ProductItem extends StatelessWidget {
           trailing: IconButton(
             icon: const Icon(Icons.shopping_cart),
             onPressed: () {
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: const Text('Produto adicionado com sucesso!'),
+                  duration: const Duration(seconds: 2),
+                  action: SnackBarAction(
+                    label: 'DESFAZER',
+                    onPressed: () {
+                      cart.removeSingleItem(product.id);
+                    },
+                  ),
+                ),
+              );
               cart.addItem(product);
             },
             color: Theme.of(context).hintColor,
